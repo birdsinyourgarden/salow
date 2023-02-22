b02f03f7895b479db1040ecf9aa53ca4dc0d1db8
fcegatti <fcegatti@yahoo.es>
2023-02-23 00:32:14 +0100

    seeder for specialities created and run

--- database/seeders/SpecialtySeeder.php
+++ database/seeders/SpecialtySeeder.php
@@ -4,14 +4,47 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Specialty;
 
 class SpecialtySeeder extends Seeder
 {
-    /**
+     /**
      * Run the database seeds.
+     *
+     * @return void
      */
     public function run(): void
     {
-        //
+      Specialty::create([
+        'name' => 'Psychology',
+    ]);
+    
+    Specialty::create([
+        'name' => 'Pediatrics',
+    ]);
+
+    Specialty::create([
+        'name' => 'Physiotherapy',
+    ]);
+
+    Specialty::create([
+        'name' => 'Dermatology',
+    ]);
+
+    Specialty::create([
+        'name' => 'Ophtalmology',
+    ]);
+
+    Specialty::create([
+        'name' => 'Dentistry',
+    ]);
+
+    Specialty::create([
+        'name' => 'Gynaechology',
+    ]);
+
+    Specialty::create([
+        'name' => 'Meditation',
+    ]);
     }
 }
