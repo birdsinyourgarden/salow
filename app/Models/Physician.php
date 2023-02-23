78805f5b6cc17cdaefe084693d35969b1155fdfd
fcegatti <fcegatti@yahoo.es>
2023-02-23 02:58:40 +0100

    Physician model created

--- app/Models/Physician.php
+++ app/Models/Physician.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Physician extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+      'name',
+      'last_name',
+      'email',
+      'phone_number',
+      'picture',
+      'description',
+      'specialty_id',
+  ];
+  
+  public function specialty() {
+    return $this->belongsTo(Specialty::class);
+  }
+
 }
