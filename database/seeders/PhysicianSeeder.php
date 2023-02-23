ec9269a09026d4b7cb95f659d282008dd3a82318
fcegatti <fcegatti@yahoo.es>
2023-02-23 07:59:32 +0100

    Physician seeder created + typo correction on Specialty seeder (on specification of type of return )

--- database/seeders/PhysicianSeeder.php
+++ database/seeders/PhysicianSeeder.php
@@ -4,14 +4,17 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Physician;
 
 class PhysicianSeeder extends Seeder
 {
     /**
      * Run the database seeds.
+     *
+     * @return void
      */
-    public function run(): void
+    public function run()
     {
-        //
+      \App\Models\Physician::factory(5)->create();
     }
 }
